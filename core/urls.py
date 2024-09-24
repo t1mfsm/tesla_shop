e20bee97b8ffa925059edcdcf84a0408e1895055
t1mfsm <shakirov.timurshakirov@yandex.ru>
2024-09-24 11:26:42 +0300

    ssr_edits

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 #from django.contrib import admin
 from django.urls import path
 
-from .views import main, product_detail, basket
+from .views import main, product_detail, car_order
 
 urlpatterns = [
     path('', main, name='main'),
-    path('products/<int:product_id>', product_detail, name='product_detail'),
-    path('basket/<int:order_id>', basket, name='basket'),
+    path('products/<str:product_name>', product_detail, name='product_detail'),
+    path('car_order/<str:order_id>', car_order, name='car_order'),
 ]
