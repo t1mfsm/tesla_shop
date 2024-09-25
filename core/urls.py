2d28c47fd7c93148538140b1eb298a17a9d3ba32
t1mfsm <shakirov.timurshakirov@yandex.ru>
2024-09-25 12:37:50 +0300

    add databse

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 #from django.contrib import admin
 from django.urls import path
 
-from .views import main, product_detail, car_order
+from .views import main, product_detail, car_order, add_product_to_order, delete_order
 
 urlpatterns = [
     path('', main, name='main'),
-    path('products/<str:product_name>', product_detail, name='product_detail'),
+    path('products/product_<int:product_id>', product_detail, name='product_detail'),
     path('car_order/<str:order_id>', car_order, name='car_order'),
+    path('add_to_order/<int:product_id>/', add_product_to_order, name='add_to_order'),
+    path('delete_order/<str:order_id>/', delete_order, name='delete_order'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,174 +1,98 @@
-URL = 'http://127.0.0.1:9000/accessories-electric-cars/{}.jpg'
-from unidecode import unidecode
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
+from .models import Product, Order, OrderStatus, OrderProduct
+from unidecode import unidecode
 from urllib.parse import unquote
+from django.utils.timezone import now
+from django.db import connection
+from django.contrib import messages
 
-PRODUCTS = [
-    {   'id': 1,
-        'name': 'Кронштейн подушки безопасности водителя',
-        'part_number': '1456163-31-A',
-        'price': '2 697 ₽',
-        'model_info': 'Tesla Model S рест. 2019',
-        'year': '2019',
-        'model': 'Model S',
-        'article_number': '99458031',
-        'brand': 'Tesla',
-        'note': 'Кронштейн подушки безопасности водителя с электропроводкой, Tesla Model S Rest, X',
-        'image': URL.format('1'),
-    },
-    {   'id': 2,
-        'name': 'Накладка колесной арки задний левый',
-        'part_number': '1035290-00-G',
-        'price': '13 485 ₽',
-        'model_info': 'Tesla Model X 2019',
-        'year': '2019',
-        'model': 'Model X',
-        'article_number': '99455421',
-        'brand': 'Tesla',
-        'note': 'Накладка колёсной арки, задней левой, оригинал, сломано одно крепление, Tesla Model X, XRest',
-        'image': URL.format('2'),
-    },
-    {   'id': 3,
-        'name': 'Подушка безопасности водителя',
-        'part_number': '1023381-00-I',
-        'price': '2 325 ₽',
-        'model_info': 'Tesla Model S 2016',
-        'year': '2016',
-        'model': 'Model S',
-        'article_number': '9895148',
-        'brand': 'Tesla',
-        'note': 'Подушка безопасности водителя (под восстановление), Tesla Model S, SR, X',
-        'image': URL.format('3'),
-    },
-    {   'id': 4,
-        'name': 'Датчик уровня пневмоподвески',
-        'part_number': '1027941-00-A',
-        'price': '3 441 ₽',
-        'model_info': 'Tesla Model X 2019',
-        'year': '2019',
-        'model': 'Model X',
-        'article_number': '994581',
-        'brand': 'Tesla',
-        'note': 'Датчик уровня пневмоподвески, передний левый, с кронштейном, Tesla Model X, S, SRest',
-        'image': URL.format('4'),
-    },
-    {   'id': 5,
-        'name': 'Усилитель заднего бампера',
-        'part_number': '1041685-00-A',
-        'price': '9 765 ₽',
-        'model_info': 'Tesla Model S рест. 2019',
-        'year': '2019',
-        'model': 'Model S',
-        'article_number': '99459388',
-        'brand': 'Tesla',
-        'note': 'Усилитель заднего бампера, Tesla Model S, SRest, SRest 2',
-        'image': URL.format('5'),
-    },
-    {   'id': 6,
-        'name': 'Кронштейн крепления ручки',
-        'part_number': '1008404-00-C',
-        'price': '465 ₽',
-        'model_info': 'Tesla Model S 2014',
-        'year': '2014',
-        'model': 'Model S',
-        'article_number': '9878558',
-        'brand': 'Tesla',
-        'note': 'Кронштейн крепления ручки открытия двери внутренней задней правой Tesla Model S, Model S REST DOOR OPENER BRACKET - RIGHT REAR',
-        'image': URL.format('6'),
-    }
-]
+def add_product_to_order(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    user = request.user
 
-ORDERS = [
-    {
-        'order_id': 1,
-        'order_date': '12.09.2024',
-        'ship_date': '15.09.2024',
-        'factory': 'Lathrop',
-        'items': [
-            {
-                'product_id': 1,
-                'quantity': 2,
-            },
-            {
-                'product_id': 3,
-                'quantity': 1,
-            }
-        ]
-    },
-    {
-        'order_id': 2,
-        'order_date': '24.09.2024',
-        'ship_date': '26.09.2024',
-        'factory': 'Fremont',
-        'items': [
-            {
-                'product_id': 2,
-                'quantity': 1,
-            },
-            {
-                'product_id': 1,
-                'quantity': 1,
-            }
-        ]
-    }
-]
+    # Проверяем наличие черновика
+    order, created = Order.objects.get_or_create(
+        creator=user,
+        status=OrderStatus.DRAFT,
+        defaults={
+            'order_date': now(),
+            'factory': 'Tesla Factory'  # Можно изменить на нужное значение
+        }
+    )
 
+    # Добавляем продукт в заявку или обновляем количество
+    order_product, created = OrderProduct.objects.get_or_create(
+        order=order, product=product, user=user,
+        defaults={'quantity': 1}
+    )
+    
+    if not created:
+        order_product.quantity += 1
+        order_product.save()
+
+    return redirect('main')
+
+def delete_order(request, order_id):
+    # Находим заказ по ID и проверяем, что заказ принадлежит текущему пользователю
+    order = get_object_or_404(Order, id=order_id, creator=request.user)
+
+    if request.method == 'POST':
+        # Удаляем заказ
+        order.delete()
+        messages.success(request, 'Ваш заказ был успешно удален.')
+        return redirect('main')  # Возвращаем пользователя на главную страницу или другую
+
+    return redirect('car_order', order_id=order_id)
 
 def main(request):
+    user = request.user
     search_product = request.GET.get('search_product', '').lower() if request.GET.get('search_product') else None
+    products = Product.objects.all()
+
     if search_product:
-        products = [product for product in PRODUCTS if search_product in product['name'].lower()]
-    else:
-        products = PRODUCTS
+        products = products.filter(name__icontains=search_product)
 
-    for product in products:
-        product['slug'] = unidecode(product['name']).replace(' ', '-')
+    current_order = Order.objects.filter(creator=user, status='draft').first()
 
-    return render(request, 'main.html', {'products': products, 'search_product': search_product})
+    has_order = current_order is not None and current_order.id is not None  # Добавлена проверка наличия id
+
+    return render(request, 'main.html', {
+        'products': products,
+        'search_product': search_product,
+        'current_order': current_order,  # Передаем заказ в шаблон
+        'has_order': has_order,
+    })
 
 
-def product_detail(request, product_name):
-    decoded_product_name = unquote(product_name)
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)  # Найти продукт по ID
     
-    decoded_product_name = unidecode(decoded_product_name).lower().replace('-', ' ')
-    
-    product = None
-    for product_item in PRODUCTS:
-        if unidecode(product_item['name']).lower() == decoded_product_name:
-            product = product_item
-            break
-
-    if not product:
-        raise Http404("Продукт не найден")
-
     return render(request, 'product_detail.html', {'product': product})
 
 
-def car_order(request, order_id):
-    if order_id.startswith("order_"):
-        numeric_order_id = int(order_id.replace("order_", ""))
-    else:
-        raise Http404("Неверный формат идентификатора заказа")
-    
-    order = next((order for order in ORDERS if order['order_id'] == numeric_order_id), None)
-    
-    if not order:
-        raise Http404("Заказ не найден")
-    
-    ordered_products = []
 
-    for order_item in order['items']:
-        for product in PRODUCTS:
-            if product['id'] == order_item['product_id']:
-                product_with_quantity = product.copy()
-                product_with_quantity['quantity'] = order_item['quantity']
-                ordered_products.append(product_with_quantity)
+def car_order(request, order_id):
+    numeric_order_id = int(order_id.replace("order_", ""))
+    order = get_object_or_404(Order, id=numeric_order_id, status=OrderStatus.DRAFT)
+
+    ordered_products = []
+    total_sum = 0
+
+    for order_item in order.order_products.all():
+        product_with_quantity = order_item.product
+        product_with_quantity.quantity = order_item.quantity
+        ordered_products.append(product_with_quantity)
+        total_sum += order_item.product.price * order_item.quantity
+
+    current_order = get_object_or_404(Order, id=order_id, creator=request.user)
 
     return render(request, 'car_order.html', {
         'products': ordered_products, 
-        'order_id': numeric_order_id,
-        'order_date': order['order_date'],
-        'ship_date': order['ship_date'],
-        'factory': order['factory']
+        'order_id': order.id,
+        'order_date': order.order_date,
+        'ship_date': order.ship_date,
+        'factory': order.factory,
+        'total_sum': total_sum,  # Добавляем итоговую сумму
+        'current_order': current_order,
     })
